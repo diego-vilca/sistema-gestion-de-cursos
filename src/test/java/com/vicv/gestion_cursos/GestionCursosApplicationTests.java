8fd40c185b3734c08c5cb8519cdaabd9f7a92e3c
vicv <vicv87.mp@gmail.com>
2026-04-30 23:55:58 -0300

    first commit

---
+++ src/test/java/com/vicv/gestion_cursos/GestionCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicv.gestion_cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
